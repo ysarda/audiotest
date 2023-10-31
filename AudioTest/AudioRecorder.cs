b2dd9cb22e7022e7926a2064606b66f1f8581760
Yash Sarda <ysarda9@gmail.com>
2023-10-31 17:44:22 -0500

    ffmpeg is working hurrah

---
+++ AudioTest/AudioRecorder.cs
@@ -0,0 +1,64 @@
+﻿// using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+class AudioRecorder
+{
+    private Process process;
+    private MemoryStream memoryStream;
+    private bool isRecording;
+
+    public AudioRecorder()
+    {
+        process = new Process();
+        process.StartInfo.FileName = "ffmpeg"; // Use "ffmpeg" or "ffmpeg.exe" based on your system configuration.
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+
+        memoryStream = new MemoryStream();
+    }
+
+    public bool IsRecording => isRecording;
+
+    public void StartRecording(string outputPath = "")
+    {
+        if (!isRecording)
+        {
+            isRecording = true;
+            memoryStream.SetLength(0); // Clear the memory stream
+            process.StartInfo.Arguments = $"-f alsa -i default {outputPath}";
+            process.Start();
+
+            process.OutputDataReceived += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    byte[] audioData = Encoding.Default.GetBytes(e.Data);
+                    memoryStream.Write(audioData, 0, audioData.Length);
+                }
+            };
+
+            process.ErrorDataReceived += (sender, e) => { /* Handle standard error if needed */ };
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+        }
+    }
+
+    public byte[] StopRecording()
+    {
+        if (isRecording)
+        {
+            isRecording = false;
+            process.Kill();
+            process.WaitForExit();
+            return memoryStream.ToArray();
+        }
+
+        return Array.Empty<byte>();
+    }
+}
--- AudioTest/Program.cs
+++ AudioTest/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AudioRecorder audioRecorder = new AudioRecorder();
+
+        Console.WriteLine("Press Enter to start recording...");
+        Console.ReadLine();
+
+        string outputPath = "recorded_audio.wav";
+
+        // Start recording
+        Console.WriteLine("Recording... Press Enter to stop recording.");
+        audioRecorder.StartRecording(outputPath);
+
+        Console.ReadLine(); // Press Enter to stop recording
+
+        // Stop recording and get the audio buffer
+        byte[] audioBuffer = audioRecorder.StopRecording();
+
+        if (audioBuffer != null)
+        {
+            Console.WriteLine($"Recording stopped. Audio saved to {outputPath}");
+
+            // Save the audio buffer to a file (for testing)
+            File.WriteAllBytes("recorded_audio_buffer.wav", audioBuffer);
+        }
+        else
+        {
+            Console.WriteLine("No audio recorded.");
+        }
+    }
+}
