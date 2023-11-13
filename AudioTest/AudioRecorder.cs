851f75020ca61e0e26f7aa1168722fab9e6489c9
Yash Sarda <ysarda9@gmail.com>
2023-11-13 09:42:05 -0600

    using class var for async proc

--- AudioTest/AudioRecorder.cs
+++ AudioTest/AudioRecorder.cs
@@ -5,17 +5,13 @@ public class AudioRecorder : IDisposable
 {
     private CancellationTokenSource _cancellationTokenSource;
     private MemoryStream _audioStream;
-    private byte[] _audioBytes;
-    private Command? _ffmpegCommand;
-    private bool _isRecording;
+    private CommandTask<CommandResult>? _ffmpegCommand;
 
     public AudioRecorder()
     {
         _cancellationTokenSource = new CancellationTokenSource();
         _audioStream = new MemoryStream();
-        _isRecording = false;
         _ffmpegCommand = null;
-        _audioBytes = Array.Empty<byte>();
     }
 
     public void StartRecording()
@@ -23,7 +19,7 @@ public class AudioRecorder : IDisposable
         _audioStream = new MemoryStream();
         _cancellationTokenSource = new CancellationTokenSource();
 
-        (Cli.Wrap("ffmpeg")
+        _ffmpegCommand = (Cli.Wrap("ffmpeg")
             .WithArguments(args => args
                 .Add("-f").Add(GetOsInput())
                 .Add("-i").Add("default")
@@ -48,7 +44,7 @@ public class AudioRecorder : IDisposable
         _audioStream.Dispose();
     }
 
-    private string GetOsInput()
+    private static string GetOsInput()
     {
         // If Mac, return avfoundation, if Linux return alsa, if Windows return dshow
         return OperatingSystem.IsMacOS() ? "avfoundation" : OperatingSystem.IsLinux() ? "alsa" : "dshow";
